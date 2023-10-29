5e6005b39621e819647757b5633c5e6995e068c7
Tyler <tylerjstocks@gmail.com>
2023-10-28 21:41:48 -0700

    Updated bad formatting in custom exceptions

--- core/exceptions.py
+++ core/exceptions.py
@@ -34,7 +34,6 @@ class InvalidVector(Exception):
   """Error raised when both the x and y componants of a vector are zero."""
 
   def __init__(self, message: str):
-    self.message = f"\b{message}\n"
     super().__init__(message)
 
 
